4ab1f8520d864830d521d4c75aef1c9206fe9b27
Aleran974 <f.pierrealexandre@live.fr>
2015-01-19 16:29:07 +0100

    envoie du site

---
+++ PizzaSaturneWeb/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style/pizz.css" />
+        <title>Pizza Saturne</title>
+    </head>
+
+    <body>
+        <header>
+
+            <div id="cadre1">
+                <p class="infos1"> Ouvert du </br>
+                    Mardi au Samedi</br>
+                    de 11h à 22h</p>
+
+            </div>
+            <div id="logo1">
+                <img alt="logo" src="image/logo.png" height="150px" width ="150px">
+            </div>
+            <div id="cadre2">
+                <p class="infos2"><u>Pizza Saturne</u><br/>
+                Tel: 05 56 46 08 59</br>
+                Pâte à Pizza fraîche et Maison</p>
+            </div>
+            <div id="logo2">
+                <img alt="logo" src="image/logo.png" height="150px" width ="150px">
+            </div>
+            <div id="cadre3">
+                <p class="infos1"> Ouvert le </br>
+                    Dimanche</br>
+                    de 18h à 22h</p>
+            </div>
+        </header>
+        <nav id="monmenu">
+            <ul class="niveau1">
+                <li>
+                    <ul class="niveau2">
+                        <li class="accueil">Accueil</li>
+                        <li id="pizza">Pizza
+                            <ul class="niveau3">
+                                <li>Base Tomate</li>
+                                <li>Base Crème</li>
+                                <li>Poissons</li>
+                                <li>A la carte</li>
+                            </ul>
+                        </li>
+                        <li>Bruchettas</li>
+                        <li>Panini</li>
+                        <li>Pâtes</li>
+                        <li>Snack</li>
+                        <li>Dessert</li>
+                        <li>Boissons</li>
+                        <li>Nos Menus</li>
+                        <li>Contact</li>
+                    </ul>
+                </li>
+            </ul>	
+        </nav>
+        <div id="annonce">
+            <div class ="platdujour">
+                <h2><u>Plat du Jour</u></h2>
+                <p>lundi : magret de canard</p>
+            </div>	
+            <div class="offredumoment">
+                <h2><u>offre du Moment</u></h2>
+                <p>Pizza Raclette</p>
+            </div>
+        </div>
+        <div id="promotion">
+            <div class="emporter">
+                <h3><u>A Emporter</u></h3>
+                <p> 3 grandes pizzas = la moins Chères offertes<br/>
+                    ou<br/>
+                    2 grandes pizzas = 17 €<p>
+            </div>
+            <div class="livraison">
+                <h3><u>Livraison</u></h3>
+                <p> 2 grandes pizzas = 19 €</p>
+                    
+            </div>
+        </div>
+        <div id ="localisation">
+            <a href="https://www.google.fr/maps/place/Pizza+Saturne/@44.807163,-0.627164,17z/data=!3m1!4b1!4m2!3m1!1s0xd54d8f6d0df81ff:0x51e401f95faf96">Plan d'accès</a>
+        </div>
+        
+        <footer>
+            <p> Copyright 2015 Pizza Saturne</p>
+        </footer>
+    </body>
+
+</html>
